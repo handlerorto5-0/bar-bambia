7dae9a96e7589a8313c73d243a84ab8934504829
excavator <str_token@mail.ru>
2019-09-02 14:04:07 +0300

    start config

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
-
-module.exports = {
-  mode: 'development',
-  entry: {},
-  output: {},
-  plugins: [],
-};
