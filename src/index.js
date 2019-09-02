235ef361d4f5bb8413f7a2c29ade7b86405a3d45
excavator <str_token@mail.ru>
2019-09-02 18:50:38 +0300

    add bootstrap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import $ from 'jquery';
 import avg from './some.js';
 import './css/style.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 $('.title').html('custom jQuery text');
 console.log(avg(1,3,5));
